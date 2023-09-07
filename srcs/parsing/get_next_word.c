f376f805d83f2fccb4541d37c1194c7fb33b4f59
Elnop <elnop@dev.fr>
2023-09-07 19:42:11 +0200

    words spliting with quotes OK

--- srcs/parsing/get_next_word.c
+++ srcs/parsing/get_next_word.c
@@ -6,7 +6,7 @@
 /*   By: lperroti <lperroti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 17:19:21 by lperroti          #+#    #+#             */
-/*   Updated: 2023/09/07 18:22:16 by lperroti         ###   ########.fr       */
+/*   Updated: 2023/09/07 19:39:26 by lperroti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static char	*get_simple_quote(char **p_line)
 		i++;
 	if (!(*p_line)[i])
 		return (NULL);
-	word = lp_substr((*p_line), 0, i - 1);
+	word = lp_substr((*p_line), 0, i);
 	*p_line += i;
 	return (word);
 }
@@ -39,7 +39,7 @@ static char	*get_double_quote(char **p_line)
 		i++;
 	if (!(*p_line)[i])
 		return (NULL);
-	word = lp_substr((*p_line), 0, i - 1);
+	word = lp_substr((*p_line), 0, i);
 	*p_line += i;
 	return (word);
 }
@@ -50,15 +50,16 @@ char	*get_next_word(char **p_line)
 	size_t	i;
 
 	if (**p_line == '\'')
-		word = get_simple_quote((p_line));
+		word = get_simple_quote(p_line);
 	else if (**p_line == '"')
 		word = get_double_quote(p_line);
 	else
 	{
 		i = 0;
-		while (p_line[i] && !lp_strchr(SPACERS, (*p_line)[i]))
+		while ((*p_line)[i] && !lp_strchr(SPACERS, (*p_line)[i]))
 			i++;
 		word = lp_substr((*p_line), 0, i);
+		*p_line += i;
 	}
 	return (word);
 }
