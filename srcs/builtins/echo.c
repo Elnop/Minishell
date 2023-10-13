bd3f2fed7d8d93deed244ce8892032d3f0444b0e
Tian SCHECROUN <schecroun.t@gmail.com>
2023-10-13 17:14:11 +0200

    hooooooooooo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: lperroti <lperroti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 20:56:42 by tschecro          #+#    #+#             */
-/*   Updated: 2023/10/12 14:15:40 by lperroti         ###   ########.fr       */
+/*   Updated: 2023/10/13 17:13:53 by tschecro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,58 @@ int	count_args(char **args)
 	return (count);
 }
 
+void	print_echo(char **args, int *i)
+{
+	while (args[*i])
+	{
+		lp_putstr_fd(args[*i], 1);
+		if (args[*i + 1] && args[*i][0] == '\0')
+			write(1, " ", 1);
+		if (args[*i + 1])
+			write(1, " ", 1);
+		(*i)++;
+	}
+}
+
+bool	check_n_option(char ** args, int *i)
+{
+	int	j;
+
+	j = 1;
+	while (args[*i] && args[*i][j])
+	{
+		if(args[*i][j] != 'n')
+			return (false);
+		j++;
+	}
+	return (true);
+}
+
 int	builtin_echo(char **args)
 {
 	int	i;
-	int	j;
 	int	n_arg;
 
 	i = 1;
-	j = -1;
 	n_arg = 0;
 	if (count_args(args) > 1)
 	{
-		if (args[i][0] == '-')
+		while (args[i] && lp_strncmp(args[i], "-n", 2) == 0)
 		{
-			while (args[i][++j])
-				if (args[i][j] != 'n')
-					return (lp_putendl_fd("echo: invalid option", 2));
+			if (i == 1)
+			{
+				if (!check_n_option(args, &i))
+					return (lp_putendl_fd("echo: invalid option\n", 2));
+			}
+			else
+			{
+				if (!check_n_option(args, &i))
+					break;
+			}
+			n_arg = 1;
 			i++;
 		}
-		while (args[i])
-		{
-			lp_putstr_fd(args[i], 1);
-			if (args[i + 1] && args[i][0] == '\0')
-				write(1, " ", 1);
-			if (args[i + 1])
-				write(1, " ", 1);
-			i++;
-		}
+		print_echo(args, &i);
 	}
 	if (n_arg == 0)
 		write(1, "\n", 1);
